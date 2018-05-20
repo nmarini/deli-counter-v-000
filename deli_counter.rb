e86837c6bbf50f022f0bba308178f96d788885c6
Nicolas Marini <nmarini44@gmail.com>
2018-05-20 21:17:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
 if katz_deli.size > 0
   current_line = "The line is currently: "
   katz_deli.each.with_index(1) do |name, i|
-    current_line << "#{i}. #{name} "
+    current_line << "#{i}. #{name}"
   end
   puts current_line
 end
